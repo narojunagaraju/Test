abf24127f242e022d85ec51f03c9275aaa8f9184
Nagaraj <android@mrmsbeauty.com>
2021-12-05 19:58:22 +0530

    initial commit

---
+++ app/src/androidTest/java/com/example/androidtesting/MainActivityTest.kt
@@ -0,0 +1,38 @@
+package com.example.androidtesting
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
+import org.junit.Assert.*
+
+import org.junit.Before
+import org.junit.Test
+import org.junit.Rule
+import org.junit.runner.RunWith
+
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+
+    @Rule
+    @JvmField
+    val mainActivityRule: ActivityTestRule<MainActivity> =
+        ActivityTestRule<MainActivity>(MainActivity::class.java)
+
+
+
+    @Test
+    fun mainActivityTest_emptyName_returnsFalse() {
+        onView(ViewMatchers.withId(R.id.editTextTextPersonName2)).perform(typeText(""))
+        onView(ViewMatchers.withId(R.id.editTextTextEmailAddress)).perform(typeText("abc@gmail.com"))
+        onView(ViewMatchers.withId(R.id.editTextTextPassword)).perform(typeText("abcde123"))
+        onView(ViewMatchers.withId(R.id.editTextTextPassword2)).perform(typeText("abcde123"))
+
+        onView(ViewMatchers.withId(R.id.button)).perform(click())
+
+    }
+
+}
---
+++ app/src/main/java/com/example/androidtesting/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.example.androidtesting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import java.util.regex.Pattern
+
+class MainActivity : AppCompatActivity() {
+    lateinit var button: Button
+    lateinit var editTextName: EditText
+    lateinit var editTextTextEmailAddress: EditText
+    lateinit var editTextPassword: EditText
+    lateinit var editTextConfirmPassword: EditText
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        initUI()
+
+
+    }
+
+    private fun initUI() {
+        button = findViewById(R.id.button)
+        editTextTextEmailAddress = findViewById(R.id.editTextTextEmailAddress)
+        editTextPassword = findViewById(R.id.editTextTextPassword)
+        editTextConfirmPassword = findViewById(R.id.editTextTextPassword2)
+        editTextName = findViewById(R.id.editTextTextPersonName2)
+        button.setOnClickListener {
+            validateRegistrationDetails(
+                editTextName.text.toString(),
+                editTextTextEmailAddress.text.toString(),
+                editTextPassword.text.toString(),
+                editTextConfirmPassword.text.toString()
+            )
+        }
+    }
+
+    public fun validateRegistrationDetails(
+        name: String,
+        emailAddress: String,
+        password: String,
+        confirmPassword: String
+    ): Boolean {
+        if (name == null || name.isEmpty()|| name.length < 6) {
+            return false
+        }
+        val emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\." +
+                "[a-zA-Z0-9_+&*-]+)*@" +
+                "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+                "A-Z]{2,7}$"
+
+        val pat: Pattern = Pattern.compile(emailRegex)
+        if (emailAddress == null ||emailAddress.isEmpty())
+            return false
+        else if (!pat.matcher(emailAddress).matches())
+            return false
+        if (password == null||password.isEmpty() || password.length < 8) {
+            return false
+        }
+        if (confirmPassword == null||confirmPassword.isEmpty() || confirmPassword != password) {
+            return false
+        }
+        return true
+    }
+}
